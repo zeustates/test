7b27b2b3b336fe404753ec252a34df342572ee5f
zeustates <carlfriedrich.kostelezky@gmail.com>
2015-07-04 13:16:21 +0200

    Create skytale.java

---
+++ skytale.java
@@ -0,0 +1,28 @@
+package krypto1;
+
+
+
+public class Skytale {
+	private String text;
+	private int ecken;
+	Skytale(String a,int b){
+		text=a;
+		ecken=b;
+		
+	}
+	public String VerschluesselterText(){
+		String ausgabe = "";
+		String[] s = new String [ecken+1];
+		
+		for (int i2=0;i2<ecken;i2++){
+			s[i2]="";
+		for (int i = i2;i<text.length();i+=ecken){
+			s[i2]=s[i2]+text.charAt(i);
+		 
+		}
+		ausgabe=ausgabe+s[i2];
+		}
+		return ausgabe;
+	}
+
+}
