1d35b6c7a3adbe520fd0db5c9cab3c92761a05d5
zeustates <carlfriedrich.kostelezky@gmail.com>
2015-07-09 16:34:59 +0200

    Update skytale.java

--- skytale.java
+++ skytale.java
@@ -3,13 +3,15 @@ package krypto1;
 
 
 public class Skytale {
-	private String text; // zu Versclüsselnder Text
-	private int ecken; //Anzahl der Seiten des imaginären Stifts 
+
+	private String text;
+	public int ecken;
 	Skytale(String a,int b){
 		text=a;
 		ecken=b;
 		
 	}
+	
 	public String VerschluesselterText(){
 		String ausgabe = "";
 		String[] s = new String [ecken+1];
@@ -24,5 +26,35 @@ public class Skytale {
 		}
 		return ausgabe;
 	}
+	//Folgender Code Funktioniert noch nicht
+	
+	public String entschluesselung(String a,int eck){
+		String []platzhalter=new String[eck+1];
+		String ausgabe ="";
+		int zaehler=0;
+		
+		int abschnitt =a.length()/eck;
+		for (int i = 0;i<eck;i++){
+			platzhalter[i]=String.valueOf(a.subSequence(i*abschnitt,i*abschnitt+abschnitt));
+			
+		}
+		int i=0;
+		for (int i2=0;i2<platzhalter[i].length();i2++){
+			for (i = 0;i<eck-1;i++){
+				zaehler++;
+				System.out.println("w");
+				if (zaehler<=a.length()){
+					ausgabe+=platzhalter[i].charAt(i2);
+			}
+
+				
+			}
+		}
+		
+		return ausgabe;
+	}
+	
+	
+
 
 }
